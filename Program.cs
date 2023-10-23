a0c1bb56f155a61ed4dc11ebad7686a478c1a20f
Dahlia Guido <dguido.dev@gmail.com>
2023-10-22 22:39:04 -0500

    practice drafts

--- Program.cs
+++ Program.cs
@@ -6,7 +6,18 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
-      Console.WriteLine("Hello, World");
+      Console.WriteLine(2 * 3);         // Basic Arithmetic: +, -, /, * {6}
+Console.WriteLine(10 % 3);        // Modulus Op => remainder of 10/3 {1}
+Console.WriteLine(1 + 2 * 3);     // order of operations  {7}
+Console.WriteLine(10 / 3.0);      // int's and doubles 
+Console.WriteLine(10 / 3);        // int's 
+Console.WriteLine("12" + "3");    // What happens here? {123}
+
+int num = 10;
+num += 100;
+Console.WriteLine(num); //110
+num ++;
+Console.WriteLine(num); //111
     }
   }
 }
