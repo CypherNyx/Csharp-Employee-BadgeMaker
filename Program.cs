5c571877a9142544ea342581dfc22fd09e738464
Dahlia Guido <dguido.dev@gmail.com>
2024-01-03 19:48:26 -0600

    set up console prompt to obtain list of employees from user input using a while loop and iterating over saved list to print on console.

--- Program.cs
+++ Program.cs
@@ -6,49 +6,24 @@ namespace CatWorx.BadgeMaker
   {
     static void Main(string[] args)
     {
-      // Dictionaries
-      Dictionary<string, int> kpopGroups = new Dictionary<string, int>(){
-        {"BTS", 7}, //Key: Group Name | Value: Number of members
-        {"BlackPink", 4},
-        {"Twice", 9},
-        {"Red Velvet", 5},
-        {"EXO", 9},
-        {"Girls Generation", 8}
-      };
-
-      Console.WriteLine("----------------");
-      Console.WriteLine("Kpop Groups");
-      Console.WriteLine("----------------");
-      Console.WriteLine("           Group | Members");
-      Console.WriteLine("             BTS | {0}", kpopGroups["BTS"] );
-      Console.WriteLine("       BlackPink | {0}", kpopGroups["BlackPink"]);
-      Console.WriteLine("           Twice | {0}", kpopGroups["Twice"]);
-      Console.WriteLine("      Red Velvet | {0}", kpopGroups["Red Velvet"]);
-      Console.WriteLine("             EXO | {0}", kpopGroups["EXO"]);
-      Console.WriteLine("Girls Generation | {0}", kpopGroups["Girls Generation"]);
-
-      // Arrays
-      string [] bangtanSongs = new string[4]{ "Spring Day", "Save Me", "Silver Spoon", "DNA"};
-      string firstSong = bangtanSongs[0];
-      string secondSong = bangtanSongs[1];
-      string thirdSong = bangtanSongs[2];
-      string lastSong = bangtanSongs[3];
-      Console.WriteLine("----------------");
-      Console.WriteLine("My Top 3 BTS Songs are {0}, {1}, {2} and my favorite song of all time is {3}.", secondSong, thirdSong, lastSong, firstSong);
-
-      // Lists
-      List<string> bangtanies = new List<string>() { "Namjoon", "Seokjin", "Yoongi", "Hoseok", "Jimin", "Taehyung", "Jungkook"};
-      Console.WriteLine("----------------");
-      Console.WriteLine("The hyung line includes {0}, {1}, {2} and {3} who is also the group's leader.", bangtanies[1], bangtanies[2], bangtanies[3], bangtanies[0]);
-      Console.WriteLine("The maknae line includes {0}, {1}, and {2}.", bangtanies[4], bangtanies[5], bangtanies[6] );
-      Console.WriteLine("----------------");
-
-      // Loops
-      for (int i = 0; i < bangtanies.Count; i++)
+      List<string> employees = new List<string>();
+      // Collect user values until the value is an empty string
+      while (true)
       {
-        Console.WriteLine(bangtanies[i]);
+        Console.WriteLine("Please enter a name: (leave empty to exit): ");
+        // Get a name from the console and assign it to a variable
+        string input = Console.ReadLine() ?? "";
+        // Break if the user hits ENTER without typing a name
+        if (input == "")
+        {
+          break;
+        }
+        employees.Add(input);
       }
-
+      for (int i = 0; i < employees.Count; i++)
+      {
+        Console.WriteLine(employees[i]);
+      }
     }
   }
 }
