27666ef0ef53ffdcd5c34b2956190f4966f18720
Dahlia Guido <dguido.dev@gmail.com>
2024-01-03 23:13:11 -0600

    Create the CSV file, loop over the employees and write the data

--- Util.cs
+++ Util.cs
@@ -26,6 +26,18 @@ namespace CatWorx.BadgeMaker
         Directory.CreateDirectory("data");
       }
 
+      using (StreamWriter file = new StreamWriter("data/employees.csv"))
+      {
+        file.WriteLine("ID,Name,PhotoUrl"); // CSV File headings
+        // Loop over employees
+        for (int i = 0; i < employees.Count; i++)
+      {
+        // Write each employee to the file
+        string template = "{0},{1},{2}";
+        file.WriteLine(String.Format(template, employees[i].GetID(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+      }
+      }
+
     }
   }
 }
