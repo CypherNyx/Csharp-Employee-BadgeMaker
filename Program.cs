40d821848b041195dce2003475129580f1edc3b3
Dahlia Guido <dguido.dev@gmail.com>
2024-01-03 21:54:05 -0600

    added remaining properties and methods to Employee Class

--- Program.cs
+++ Program.cs
@@ -10,16 +10,23 @@ namespace CatWorx.BadgeMaker
       // Collect user values until the value is an empty string
       while (true)
       {
-        Console.WriteLine("Please enter a name: (leave empty to exit): ");
+        Console.WriteLine("Please enter first name: (leave empty to exit): ");
         // Get a name from the console and assign it to a variable
-        string input = Console.ReadLine() ?? "";
+        string firstName = Console.ReadLine() ?? "";
         // Break if the user hits ENTER without typing a name
-        if (input == "")
+        if (firstName == "")
         {
           break;
         }
+        Console.Write("Enter last name: ");
+        string lastName = Console.ReadLine() ?? "";
+        Console.Write("Enter ID: ");
+        int id = Int32.Parse(Console.ReadLine() ?? "");
+        Console.Write("Enter Photo URL: ");
+        string photoUrl = Console.ReadLine() ?? "";
+
         // Create a new Employee instance
-        Employee currentEmployee = new Employee(input, "guido");
+        Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
         employees.Add(currentEmployee);
       }
       return employees;
@@ -29,7 +36,8 @@ namespace CatWorx.BadgeMaker
     {
       for (int i = 0; i < employees.Count; i++)
       {
-        Console.WriteLine(employees[i].GetFullName());
+        string template = "{0,-10}\t{1,-20}\t{2}";
+        Console.WriteLine(String.Format(template, employees[i].GetID(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
       }
     }
 
