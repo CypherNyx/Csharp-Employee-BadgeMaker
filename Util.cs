94a480c91ad26b6cbc5e1c8e0817fbc8f2ef9bfe
Dahlia Guido <dguido.dev@gmail.com>
2024-01-04 22:55:10 -0600

    print employee information to badge

--- Util.cs
+++ Util.cs
@@ -7,7 +7,8 @@ using SkiaSharp;
 
 namespace CatWorx.BadgeMaker
 {
-  class Util{
+  class Util
+  {
     // Print employees data
     public static void PrintEmployees(List<Employee> employees)
     {
@@ -19,7 +20,7 @@ namespace CatWorx.BadgeMaker
 
     }
 
-// make the CSV file
+    // make the CSV file
     public static void MakeCSV(List<Employee> employees)
     {
       // Check to see if folder exists
@@ -34,16 +35,16 @@ namespace CatWorx.BadgeMaker
         file.WriteLine("ID,Name,PhotoUrl"); // CSV File headings
         // Loop over employees
         for (int i = 0; i < employees.Count; i++)
-      {
-        // Write each employee to the file
-        string template = "{0},{1},{2}";
-        file.WriteLine(String.Format(template, employees[i].GetID(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
-      }
+        {
+          // Write each employee to the file
+          string template = "{0},{1},{2}";
+          file.WriteLine(String.Format(template, employees[i].GetID(), employees[i].GetFullName(), employees[i].GetPhotoUrl()));
+        }
       }
     }
 
     // make the badges
-     async public static Task MakeBadges(List<Employee> employees)
+    async public static Task MakeBadges(List<Employee> employees)
     {
       //Layout variables
       int BADGE_WIDTH = 669;
@@ -54,10 +55,16 @@ namespace CatWorx.BadgeMaker
       int PHOTO_RIGHT_X = 486;
       int PHOTO_BOTTOM_Y = 517;
 
+      int COMPANY_NAME_Y = 150;
+
+      int EMPLOYEE_NAME_Y = 600;
+
+      int EMPLOYEE_ID_Y = 730;
+
       // instance of HttpClient is disposed after code in the block has run
-      using(HttpClient client = new HttpClient())
+      using (HttpClient client = new HttpClient())
       {
-        for (int i = 0; i < employees.Count; i++ )
+        for (int i = 0; i < employees.Count; i++)
         {
           SKImage photo = SKImage.FromEncodedData(await client.GetStreamAsync(employees[i].GetPhotoUrl()));
           SKImage background = SKImage.FromEncodedData(File.OpenRead("badge.png"));
@@ -65,8 +72,30 @@ namespace CatWorx.BadgeMaker
           SKBitmap badge = new SKBitmap(BADGE_WIDTH, BADGE_HEIGHT);
           SKCanvas canvas = new SKCanvas(badge);
 
-          canvas.DrawImage(background, new SKRect (0,0, BADGE_WIDTH, BADGE_HEIGHT));
-          canvas.DrawImage(photo, new SKRect(PHOTO_LEFT_X, PHOTO_TOP_Y, PHOTO_RIGHT_X, PHOTO_BOTTOM_Y ));
+          canvas.DrawImage(background, new SKRect(0, 0, BADGE_WIDTH, BADGE_HEIGHT));
+          canvas.DrawImage(photo, new SKRect(PHOTO_LEFT_X, PHOTO_TOP_Y, PHOTO_RIGHT_X, PHOTO_BOTTOM_Y));
+
+
+          SKPaint paint = new SKPaint();
+          paint.TextSize = 42.0f;
+          paint.IsAntialias = true;
+          paint.Color = SKColors.White;
+          paint.IsStroke = false;
+          paint.TextAlign = SKTextAlign.Center;
+          paint.Typeface = SKTypeface.FromFamilyName("Arial");
+
+          // Company name
+          canvas.DrawText(employees[i].GetCompanyName(), BADGE_WIDTH / 2f, COMPANY_NAME_Y, paint);
+
+          paint.Color = SKColors.Black;
+
+          // Employee name
+          canvas.DrawText(employees[i].GetFullName(), BADGE_WIDTH / 2f, EMPLOYEE_NAME_Y, paint);
+
+          paint.Typeface = SKTypeface.FromFamilyName("Courier New");
+
+          // Employee ID
+          canvas.DrawText(employees[i].GetID().ToString(), BADGE_WIDTH / 2f, EMPLOYEE_ID_Y, paint);
 
           SKImage finalImage = SKImage.FromBitmap(badge);
           SKData data = finalImage.Encode();
